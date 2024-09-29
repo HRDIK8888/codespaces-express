adc8aedff790271e705b1e1019d75293e0c58ca1
Hardik Vijay Borle <hardikborle22@gmail.com>
2024-09-29 13:50:33 +0000

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import { connect, Mongoose } from "mongoose";
 import { DB_NAME } from "./constant"
 import { connectDB } from "./db/index.js";
+import { application } from "express";
 
 dotenv.config({
  path: './env'
@@ -12,5 +13,15 @@ dotenv.config({
 
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000,() => {
+        console.log("server is running");
+        
+    })
+})
+.catch((err) =>{
+    console.log("MONGO db connection failed !!!,err");
+    
+})
 
 
